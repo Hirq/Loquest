2203156f28d8c7c3d363d35c2a54cf80fe1ae5de
Cezary Zabrocki <czarkooo@gmail.com>
2018-05-12 03:26:06 +0200

    feat: CSS - Layout

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/todo/static/todo/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'todo'),  # Here you tell django to look for a folder named 'assets'
+    ]
 
 LOGIN_REDIRECT_URL = '/todo/'
 
--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "todo"
 urlpatterns = [
@@ -10,5 +11,6 @@ urlpatterns = [
     path('<int:quest_id>/choice_form/', views.new_log, name='create_log'),
     path('<int:pk>/quest_confirm_delete/', views.QuestDelete.as_view(), name='quest_confirm_delete'),
     path('about', views.about, name='about'),
-    path('<int:pk>/quest_update_form/', views.DoneQuest.as_view(), name='quest_update_form')
-]
+    path('<int:pk>/quest_update_form/', views.DoneQuest.as_view(), name='quest_update_form'),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
